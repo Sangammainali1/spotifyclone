223a265388c9c6735020be1615f50b654666727b
Sangam Mainali <123075590+Sangammainali1@users.noreply.github.com>
2023-12-28 21:57:11 +0545

    some javascript code written but doesnot run successfully

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello sangam mainali .. YOu are op")
+
+
+
+async function main(){
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = document.getElementsByTagName("a")    
+    console.log(as)
+
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+        
+    }
+
+    console.log(songs)
+
+}
+main()
