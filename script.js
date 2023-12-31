a808c323de20edfb25c96cfe0f7c894ebabf3ca9
Sangam Mainali <123075590+Sangammainali1@users.noreply.github.com>
2023-12-31 12:49:42 +0545

    js corrected there was typo

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 
-
-
-async function main(){
+async function main() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
     console.log(response);
+
+    // Create a temporary div to hold the HTML content
     let div = document.createElement("div");
     div.innerHTML = response;
-    let as = document.getElementsByTagName("a")    
-    console.log(as)
 
-    let songs = []
+    // Get the anchor elements within the dynamically created div
+    let as = div.getElementsByTagName("a");
+    console.log(as);
+
+    let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href);
         }
-        
     }
 
-    console.log(songs)
-
+    console.log(songs);
 }
-main()
+
+// Call the main function
+main();
+
+
+
+
+
+
+
+
+
+
+
+
