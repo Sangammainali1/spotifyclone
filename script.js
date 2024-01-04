0d21b4bd44046c962103b664fc4c0fc1d7bcc6e3
Sangam Mainali <123075590+Sangammainali1@users.noreply.github.com>
2024-01-04 19:01:36 +0545

    seekbar design added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-async function main() {
+async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
     console.log(response);
@@ -16,15 +16,52 @@ async function main() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href);
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
 
-    console.log(songs);
+    // console.log(songs);
+    return songs
 }
 
-// Call the main function
-main();
+// getSongs()
+
+async function main(){
+    //  get the list of all songs
+
+    let songs = await getSongs()
+    console.log(songs)
+
+
+    //  show all the song in the playlist
+    
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+    songUl.innerHTML = songUl.innerHTML + `<li> 
+    <img src="music.svg" width="20" class="invert" alt="music">
+    <div class="info">
+      <div>${song.replaceAll("%20"," ")}</div>
+      <div>Sangam</div>
+    </div>
+    <div class="playNow">
+      <span>Play Now</span>
+      <img class="invert" src="play.svg" alt="play">
+    </div>
+  </li>
+    
+    
+    
+    
+    
+    </li>`        
+    }
+
+    // play the first songs
+    var audio = new Audio(songs[0])
+    // audio.play()
+}
+
+main()
 
 
 
